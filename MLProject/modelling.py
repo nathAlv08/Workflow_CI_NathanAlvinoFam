2f255961712932f9131f69813e7c5ca3122a3af3
nathAlv08 <nathanalvinofam@gmail.com>
2026-04-21 21:20:28 +0700

    Force log model explicitly to fix docker build

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,7 +6,6 @@ import mlflow
 
 def main():
     print("Memuat dataset untuk Workflow CI...")
-    # Path relatif terhadap file modelling.py
     data_path = "dataset_preprocessing/credit_risk_clean.csv"
     
     if not os.path.exists(data_path):
@@ -19,18 +18,18 @@ def main():
     y = df['loan_status']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    # Autolog tetap dinyalakan untuk mencatat metrik
     mlflow.sklearn.autolog()
 
-    # mlflow.start_run() akan otomatis nyambung ke "Bos" (mlflow run)
     with mlflow.start_run() as run:
         print("Melatih model di environment MLflow Project...")
         rf = RandomForestClassifier(n_estimators=100, random_state=42)
         rf.fit(X_train, y_train)
+        
+        # BARIS SAKTI: Memaksa MLflow membungkus dan menyimpan modelnya
+        mlflow.sklearn.log_model(rf, "model")
+        
         print(f"Training Selesai! Run ID: {run.info.run_id}")
-
-        # Simpan Run ID ke file teks agar bisa dibaca oleh GitHub Actions
-        with open("run_id.txt", "w") as f:
-            f.write(run.info.run_id)
 
 if __name__ == "__main__":
     main()
